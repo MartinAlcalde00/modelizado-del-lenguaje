3f21bc01d31e4dacf4b105df2f11dd649849b620
marti <martin.alcalde2000@gmail.com>
2021-12-21 20:55:34 +0100

    final version

--- modelizacion2.py
+++ modelizacion2.py
@@ -2,17 +2,21 @@
 import nltk
 from nltk.corpus import stopwords
 
-stopwords = stopwords.words('spanish')
+stopwords = stopwords.words('english')
+def remove_symbols(str):
+    str1 = str.replace('.', '').replace(',', '').replace(';', '')
+    str2 = str1.replace('(', '').replace(')', '').replace('``', '')
+    return str2
 def conditioning_file(file_name):
         huge_list = [] 
         with open(file_name, "r") as f:
             huge_list = f.read().split()
         huge_list_no_stopwords = [word.lower() for word in huge_list
                          if word.lower() not in stopwords]
-        text_no_stopwords = ' '.join(huge_list_no_stopwords).replace('.', '').replace(',', '')
+        text_no_stopwords = remove_symbols(' '.join(huge_list_no_stopwords))
         return text_no_stopwords
     
-tokens = nltk.word_tokenize(conditioning_file('test.txt'))
+tokens = nltk.word_tokenize(conditioning_file('kafka.txt'))
 
 #Create your bigrams
 bgs = nltk.bigrams(tokens)
@@ -21,10 +25,12 @@ fdist = nltk.FreqDist(bgs)
 #Search and frequencies
 sorted_by_second = sorted(fdist.items(), 
                           key = lambda tup: tup[1], reverse = True)
-search_word = 'encontrar'
+search_word = 'body'
 for k,v in sorted_by_second:
     if search_word in k[0]:
         print(k,v)
     else:
         None
+        
+        
         
--- modelizacion3.py
+++ modelizacion3.py
@@ -1,25 +1,30 @@
-# Natural Language Toolkit
 import math
 import nltk
 from nltk.corpus import stopwords
 
-stopwords = stopwords.words('spanish')
+stopwords = stopwords.words('english') #opcional con el método del punto de transición
+def remove_symbols(str):
+    str1 = str.replace('.', '').replace(',', '').replace(';', '')
+    str2 = str1.replace('(', '').replace(')', '').replace('"', '')
+    return str2
 def conditioning_file(file_name):
         huge_list = [] 
         with open(file_name, "r") as f:
             huge_list = f.read().split()
         huge_list_no_stopwords = [word.lower() for word in huge_list
                          if word.lower() not in stopwords]
-        text_no_stopwords = ' '.join(huge_list_no_stopwords).replace('.', '').replace(',', '')
+        text_no_stopwords = remove_symbols(' '.join(huge_list_no_stopwords))
         return text_no_stopwords
-    
-tokens = nltk.word_tokenize(conditioning_file('test.txt'))
-#Transition point
-pt = math.sqrt(len(tokens))
-#Create your n-grams
+
+
+tokens = nltk.word_tokenize(conditioning_file('kafka.txt'))
+
+# Create your n-grams
 ugs = nltk.FreqDist(tokens)
 bgs = nltk.bigrams(tokens)
 fdist = nltk.FreqDist(bgs)
+# punto de transición
+pt = math.sqrt(len(ugs))
 
 def medida(repeticiones, punto_transicion):
     medir = 1 / (abs(float(repeticiones) - punto_transicion) + 1)
@@ -29,21 +34,18 @@ def take_second(elem):
     return elem[1]
 
 con_medida = list()
-search_word = 'encontrar'
-sorted_by_second = sorted(fdist.items(), 
-                          key = lambda tup: tup[1], reverse=True)
-
-for k,v in sorted_by_second:
-    if search_word in k[0]:
-        for word2 in k[1]:
-            if word2 != search_word:
-                for w, freq in ugs.items():
-                    if w == word2:
-                        m = medida(freq, pt)
+search_word = 'body' #palabra intruducida
+for k, v in fdist.items():
+    if search_word in k:
+        for word2 in k:
+            if (word2 != search_word) and (search_word == k[0]):
+                for w, times in ugs.items():
+                    if w == word2 :
+                        m = medida(times, pt)
                         con_medida.append([k, m])
 
-ordenados = sorted(con_medida, key = take_second, reverse = True)
 
-#ordenados_sin_repeticion = list(dict.fromkeys(ordenados))
-#print(ordenados_sin_repeticion)
-        
+ordenados = sorted(con_medida, key = take_second, reverse = True)
+for i in range(len(ordenados)):
+    print(ordenados[i])
+
